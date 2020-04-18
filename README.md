9255b6bbf54af565af41a3c1d1e6bb48e0cbddb3
Judyll Agan <judyll.agan@outlook.com>
2020-04-18 13:37:17 +0800

    Adding the zip operator.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ScotchRxjsCombine
+SOURCE: https://scotch.io/tutorials/rxjs-operators-for-dummies-forkjoin-zip-combinelatest-withlatestfrom
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+// SOURCE: https://scotch.io/tutorials/rxjs-operators-for-dummies-forkjoin-zip-combinelatest-withlatestfrom
+
 import { Component, OnInit } from '@angular/core';
 
 /* 0. Import RXJS Operators */
@@ -18,7 +20,52 @@ type Logo = 'fish' | 'dog' | 'bird' | 'cow';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  ngOnInit() {
+  /* 2. Create the two persons - color and logo observables.
+        They will communicate with us later (when we subscribe) */
+  private color$ = new Subject<Color>();
+  private logo$ = new Subject<Logo>();
 
+  constructor() {}
+
+  ngOnInit() {
+    /* 3. We are ready to start printing shirt. Need to subscribe
+    color and log observables to produce shirts, we will write code
+    here later.
+    */
+
+    /* 3.1 zip - love bird (you jump, i jump)
+    zip operator is the love birds operator. In our case, color will wait for logo
+    whenever there are new value (vice versa). Both values must change then only
+    the log gets triggered.
+    1. Ms. Color picks WHITE
+    2. Mr. Logo picks FISH <- log 01, WHITE + FISH in pair, love birds!
+    3. Ms. Color picks GREEN
+    4. Mr. Logo picks DOG <- log 02, GREEN + DOG in pair, love birds!
+    5. Ms. Color picks RED
+    6. Mr. Logo picks BIRD <- log 03, RED + BIRD in pair love birds!
+    7. Ms. Color picks BLUE <- waiting for love...
+    */
+    zip(this.color$, this.logo$).subscribe(([color, logo]) => {
+      console.log('Zip => ', `${color} shirt with ${logo}`);
+    });
+
+    /* 4. The two persons(observables) are doing their job, picking color and logo
+    */
+    this.color$.next('white');
+    this.logo$.next('fish');
+
+    this.color$.next('green');
+    this.logo$.next('dog');
+
+    this.color$.next('red');
+    this.logo$.next('bird');
+
+    this.color$.next('blue');
+
+    /* 5. When the two persons(observables) has no more info, they say bye, bye
+      We will write the codes here later. */
+    this.color$.complete();
+    this.logo$.complete();
   }
+
 }
